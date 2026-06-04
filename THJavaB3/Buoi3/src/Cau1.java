82cc157cc6c5f98a2233c959ee6179d1d8aa401c
NguyenKhang <nguyenkhang1642@gmail.com>
2026-06-04 08:15:20 +0700

    Them B3

---
+++ THJavaB3/Buoi3/src/Cau1.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Cau1{
+
+    static void Arrayinput(int[] arr, int n){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so luong phan tu: ");
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+    public static void main(String[] args){
+
+    }
+} 
